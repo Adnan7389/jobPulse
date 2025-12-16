a63a5154f6b5475adc0d957124d7b0f0a7e3ac1a
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-16 22:43:07 +0300

    feat(users): register User model in admin

---
+++ backend/apps/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'telegram_id', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('JobPulse Info', {'fields': ('telegram_id', 'skills', 'preferences')}),
+    )

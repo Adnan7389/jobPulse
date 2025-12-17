695a44c4e966e2112b861f5ce6c9463f1c637856
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-17 17:38:16 +0300

    feat(jobs): add JobPostSerializer

---
+++ backend/apps/jobs/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import JobPost
+from apps.channels.models import Channel
+
+class JobPostSerializer(serializers.ModelSerializer):
+    channel_name = serializers.CharField(source='channel.name', read_only=True)
+    channel_id = serializers.PrimaryKeyRelatedField(
+        queryset=Channel.objects.all(), source='channel', write_only=True
+    )
+
+    class Meta:
+        model = JobPost
+        fields = [
+            'id', 'channel_id', 'channel_name', 'message_id', 
+            'raw_text', 'clean_text', 'source_link', 'is_processed'
+        ]
+        read_only_fields = ['is_processed', 'clean_text', 'id']

e966ed6ee5557ee6ab7dc36ad653d84701fd3421
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-21 15:28:49 +0300

    feat(backend): update User views to support filtering and details

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,9 +1,22 @@
 from rest_framework import generics
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 
 User = get_user_model()
 
-class UserCreateView(generics.CreateAPIView):
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        """Allow filtering by telegram_id"""
+        queryset = User.objects.all()
+        telegram_id = self.request.query_params.get('telegram_id')
+        if telegram_id:
+            queryset = queryset.filter(telegram_id=telegram_id)
+        return queryset
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer

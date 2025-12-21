56c415db60b77d3b6c09d30bd87a07bd53fb57fb
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-21 15:34:53 +0300

    feat(backend): add health check endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/', health_check),
     path('api/users/', include('apps.users.urls')),
     path('api/channels/', include('apps.channels.urls')),
     path('api/job_posts/', include('apps.jobs.urls')),

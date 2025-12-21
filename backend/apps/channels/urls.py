eb1d8c4de6898d8719aa8825a4f01abbdf6ee3c1
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-21 15:29:05 +0300

    feat(backend): update Channel URLs for detail view

--- backend/apps/channels/urls.py
+++ backend/apps/channels/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChannelCreateListView
+from .views import ChannelCreateListView, ChannelDetailView
 
 urlpatterns = [
     path('', ChannelCreateListView.as_view(), name='channel-list-create'),
+    path('<int:pk>/', ChannelDetailView.as_view(), name='channel-detail'),
 ]

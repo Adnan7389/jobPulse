9dc258c18914e5114a7213f3a092c4000f55b976
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-12-23 22:09:28 +0300

    views: trigger background task on job post creation

--- backend/apps/jobs/views.py
+++ backend/apps/jobs/views.py
@@ -2,6 +2,12 @@ from rest_framework import generics
 from .models import JobPost
 from .serializers import JobPostSerializer
 
+from .tasks import process_new_job_post
+
 class JobPostCreateView(generics.CreateAPIView):
     queryset = JobPost.objects.all()
     serializer_class = JobPostSerializer
+
+    def perform_create(self, serializer):
+        job = serializer.save()
+        process_new_job_post.delay(job.id)
